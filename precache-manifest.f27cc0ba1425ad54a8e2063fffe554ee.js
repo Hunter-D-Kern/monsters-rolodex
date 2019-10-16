9680449ba6211887d95c322628efb7af44351fcc
Hunter Kern <Hunterkern2015@u.northwestern.edu>
2019-10-16 12:26:20 -0500

    Updates

---
+++ precache-manifest.f27cc0ba1425ad54a8e2063fffe554ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7543e2a5479b7bcb517afe9e30c5bee7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "368d0f8f4adbd5ecc298",
+    "url": "/monsters-rolodex/static/css/main.a4e0d3b0.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "368d0f8f4adbd5ecc298",
+    "url": "/monsters-rolodex/static/js/main.546e3b36.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
